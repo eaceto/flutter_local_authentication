225dd87fbbdebad3a4fb41da58958944dae67d95
Ezequiel L. Aceto <ezequiel.aceto@gmail.com>
2021-03-05 23:45:52 -0300

    Added android implementation of 'authenticate' operation

--- android/src/main/kotlin/com/example/flutter_local_authentication/FlutterLocalAuthenticationPlugin.kt
+++ android/src/main/kotlin/com/example/flutter_local_authentication/FlutterLocalAuthenticationPlugin.kt
@@ -1,12 +1,10 @@
 package com.example.flutter_local_authentication
 
-import android.content.Context
-import android.hardware.fingerprint.FingerprintManager
 import androidx.annotation.NonNull
 import androidx.biometric.BiometricManager
-import androidx.core.hardware.fingerprint.FingerprintManagerCompat
-import io.flutter.embedding.android.FlutterActivity
-
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.embedding.engine.plugins.activity.ActivityAware
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
@@ -16,57 +14,83 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** FlutterLocalAuthenticationPlugin */
-class FlutterLocalAuthenticationPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
-  private lateinit var channel : MethodChannel
-  private var activity: FlutterActivity? = null
+class FlutterLocalAuthenticationPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
+    private lateinit var channel: MethodChannel
+    private var activity: FlutterFragmentActivity? = null
 
-  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_local_authentication")
-    channel.setMethodCallHandler(this)
-  }
-
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getSupportsAuthentication") {
-
-      if (activity != null) {
-        val supportsBiometrics = if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-          val biometricManager = BiometricManager.from(activity!!)
-          when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG
-                  or BiometricManager.Authenticators.DEVICE_CREDENTIAL
-                  or BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
-            BiometricManager.BIOMETRIC_SUCCESS -> true
-            else -> false
-          }
-        } else {
-          val fingerprintManager: FingerprintManager = activity!!.context.getSystemService(Context.FINGERPRINT_SERVICE) as FingerprintManager
-          fingerprintManager.isHardwareDetected && fingerprintManager.hasEnrolledFingerprints()
-        }
-        result.success(supportsBiometrics)
-        return
-      }
-      result.success(false)
-    } else {
-      result.notImplemented()
+    override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_local_authentication")
+        channel.setMethodCallHandler(this)
     }
-  }
 
-  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-    channel.setMethodCallHandler(null)
-  }
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+          "getSupportsAuthentication" -> result.success(supportsAuthentication())
+          "authenticate" -> authenticate(result)
+            else -> result.notImplemented()
+        }
+    }
 
-  override fun onAttachedToActivity(binding: ActivityPluginBinding) {
-    activity = binding.activity as? FlutterActivity
-  }
+    private fun supportsAuthentication(): Boolean {
+        activity?.let {
+            val biometricManager = BiometricManager.from(it)
+            return when (biometricManager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_STRONG
+                    or BiometricManager.Authenticators.DEVICE_CREDENTIAL
+                    or BiometricManager.Authenticators.BIOMETRIC_WEAK)) {
+              BiometricManager.BIOMETRIC_SUCCESS -> true
+                else -> false
+            }
+        }
+        return false
+    }
 
-  override fun onDetachedFromActivityForConfigChanges() {
-    activity = null
-  }
+    private fun authenticate(@NonNull result: Result) {
+        activity?.let {
+            val executor = ContextCompat.getMainExecutor(it)
+            val biometricPrompt = BiometricPrompt(it, executor,
+                    object : BiometricPrompt.AuthenticationCallback() {
+                      override fun onAuthenticationError(errorCode: Int,
+                                                         errString: CharSequence) {
+                        super.onAuthenticationError(errorCode, errString)
+                        result.error("authentication_error_$errorCode", "$errString", null)
+                      }
 
-  override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
-    activity = binding.activity as? FlutterActivity
-  }
+                      override fun onAuthenticationSucceeded(
+                              authResult: BiometricPrompt.AuthenticationResult) {
+                        super.onAuthenticationSucceeded(authResult)
+                        result.success(true)
+                      }
 
-  override fun onDetachedFromActivity() {
-    activity = null
-  }
+                    })
+
+            val promptInfo = BiometricPrompt.PromptInfo.Builder()
+                    .setTitle("Biometric login for my app")
+                    .setSubtitle("Log in using your biometric credential")
+                    .setNegativeButtonText("Use account password")
+                    .build()
+
+            biometricPrompt.authenticate(promptInfo)
+        }
+        result.error("null_pointer_exception", "Activity is null.", null)
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
+
+    override fun onAttachedToActivity(binding: ActivityPluginBinding) {
+        activity = binding.activity as? FlutterFragmentActivity
+    }
+
+    override fun onDetachedFromActivityForConfigChanges() {
+        activity = null
+    }
+
+    override fun onReattachedToActivityForConfigChanges(binding: ActivityPluginBinding) {
+        activity = binding.activity as? FlutterFragmentActivity
+    }
+
+    override fun onDetachedFromActivity() {
+        activity = null
+    }
 }
--- example/android/app/src/main/kotlin/com/example/flutter_local_authentication_example/MainActivity.kt
+++ example/android/app/src/main/kotlin/com/example/flutter_local_authentication_example/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.flutter_local_authentication_example
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity : FlutterFragmentActivity()
