fa81cac76d0d4c28fde942ba576f925c77caeed4
Earl Grant Reyes <earl.r@affinidi.com>
2023-09-12 19:42:05 +0800

    fix: show biometrics prompt everytime `authenticate` is invoked

--- macos/Classes/FlutterLocalAuthenticationPlugin.swift
+++ macos/Classes/FlutterLocalAuthenticationPlugin.swift
@@ -3,9 +3,7 @@ import LocalAuthentication
 import FlutterMacOS
 
 public class FlutterLocalAuthenticationPlugin: NSObject, FlutterPlugin {
-    
-    let context = LAContext()
-    
+
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "flutter_local_authentication", binaryMessenger: registrar.messenger)
         let instance = FlutterLocalAuthenticationPlugin()
@@ -13,13 +11,15 @@ public class FlutterLocalAuthenticationPlugin: NSObject, FlutterPlugin {
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        let context = LAContext()
         context.touchIDAuthenticationAllowableReuseDuration = 0
+
         switch call.method {
         case "supportsAuthentication":
-            let (supports, error) = supportsLocalAuthentication(with: .deviceOwnerAuthentication)
+            let (supports, error) = supportsLocalAuthentication(with: .deviceOwnerAuthentication, context: context)
             result(supports && error == nil)
         case "authenticate":
-            authenticate() { autheticated, error in
+            authenticate(with: .deviceOwnerAuthentication, context: context) { autheticated, error in
                 if let error = error {
                     let flutterError = FlutterError(code: "authentication_error", message: error.localizedDescription, details: nil)
                     result(flutterError)
@@ -32,13 +32,13 @@ public class FlutterLocalAuthenticationPlugin: NSObject, FlutterPlugin {
         }
     }
 
-    fileprivate func supportsLocalAuthentication(with policy: LAPolicy) -> (Bool, Error?) {
+    fileprivate func supportsLocalAuthentication(with policy: LAPolicy, context: LAContext) -> (Bool, Error?) {
         var error: NSError?
         let supportsAuth = context.canEvaluatePolicy(policy, error: &error)
         return (supportsAuth, error)
     }
 
-    fileprivate func authenticate(with policy: LAPolicy = .deviceOwnerAuthentication, callback: @escaping (Bool, Error?) -> Void) {
+    fileprivate func authenticate(with policy: LAPolicy, context: LAContext, callback: @escaping (Bool, Error?) -> Void) {
         let reason = "validate your user's session."
         context.evaluatePolicy(policy, localizedReason: reason, reply: callback)
     }
